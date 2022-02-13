6806348ead80cbaf9a6472aa86d6ccc576d399f8
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 11:37:28 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -2,7 +2,7 @@ function familiarisation() {
 return <div><h1>Answer #1</h1>
 <p>5 most favourite:</p>
 <p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis<br></br>2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
-confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I've been doing this for a long and would like to continue doing so.<br></br>.4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to &#39;one team one dream&#39;<br></br>.</p>
+confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization. I've been doing this for a long and would like to continue doing so.<br></br>.4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to &#39;one team one dream&#39;<br></br>.</p>
 </div>
 }
 
