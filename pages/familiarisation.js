e5f70af5b953895254acd99a844f38be56737d50
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 10:14:35 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -5,9 +5,9 @@ return <div><h1>Answer #1</h1>
 1.Dig through logs to troubleshoot a customer's broken project - I love troubleshooting and analysis
 2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
 confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI 
-3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I've been doing this for a long and would like to continue doing so.
-4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to 'one team one dream'
-5.Work with people to figure out if Vercel is suitable for their use case - Client use case is where it all starts + knowing the client's use case always helps in understanding how they are using our offerings.
+3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I have been doing this for a long and would like to continue doing so.
+4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to one team one dream
+5.Work with people to figure out if Vercel is suitable for their use case - Client use case is where it all starts + knowing the client use case always helps in understanding how they are using our offerings.
 </p></div>
 }
 
