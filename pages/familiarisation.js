f12e55867e901db91163fe5e03c5fdb18deeb431
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 11:25:16 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -1,7 +1,8 @@
 function familiarisation() {
 return <div><h1>Answer #1</h1>
 <p>5 most favourite:</p>
-<p>1.Dig through logs to troubleshoot a customer&#39;s broken project I love troubleshooting and analysis</p>
+<p>1.Dig through logs to troubleshoot a customer&#39;s broken project I love troubleshooting and analysis<br></br>2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
+confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.</p>
 </div>
 }
 
