167a5d42060fb855ff553f9324c3e656d5f874c5
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 11:01:13 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -1,8 +1,8 @@
 function familiarisation() {
 return <div><h1>Answer #1
 <p>5 most favourite:</p>
-<p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis
-they are using our offerings.</p></div>
+<p1>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis
+they are using our offerings.</p1></div>
 }
 
 export default familiarisation
