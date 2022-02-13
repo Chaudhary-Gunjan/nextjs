666de36efbf900e3e73ea63939e6371cc0f94a31
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 16:39:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
 }
+{
+  "redirects": [
+    { 
+      "source": "/redirect",
+      "destination": "https://vercel.com/", 
+      "permanent": true
+    }
+  ]
+}
