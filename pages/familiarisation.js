3232c46e59edb8930b460c1410f304de08f24510
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 08:58:34 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -2,7 +2,7 @@ function familiarisation() {
  
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
-return <div>familiarisation-Exercise</div>
+return <div>familiarisation-Exercise1</div>
 }
 
 export default familiarisation
