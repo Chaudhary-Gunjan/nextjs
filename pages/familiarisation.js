37a7325462723e520553cb25431c05cc099584b2
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-13 12:07:41 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -8,15 +8,9 @@ confidence of a number of clients in our offering is always very helpful as most
 <p>5 Least Favourite:</p>
 
 
-<p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis<br></br>2.Analyze hundreds of support 
-tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
-confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very 
-high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any 
-organization/team. I&#39;ve been doing this for a long and would like to continue doing so.
-<br></br>4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates 
-which is very helpful when it comes to &#39;one team one dream&#39; <br></br>5.Work with people to figure out if Vercel is suitable for their use 
-case - Client use case is where it all starts + knowing the client&#39;s use case always helps in understanding 
-how they are using our offerings.</p></div>
+<p>1.Manage a support team- I am happy to lead a team but managing a team is responsibilty that I&#39;d like to avoid as at times mamagers struggle to be hundred percent transperent with the team<br></br>2.
+Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites - something I&#39;like to avoid for now as my written commmunication is not up to mark to represent an organization at platform
+where my comments would be readed by tens of thousands of people.</p></div>
 
 
 }
