00c9044436564363ac3c2ced2452f828b44bd357
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-15 16:45:04 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -1,5 +1,5 @@
 function familiarisation() {
-return <div><h1>Answer #Meera</h1>
+return <div><h1>Answer #1</h1>
 <p>5 Most Favourite:</p>
 <p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis<br></br>2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
 confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I&#39;ve been doing this for a long and would like to continue doing so.
