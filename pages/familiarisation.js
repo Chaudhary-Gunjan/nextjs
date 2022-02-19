f591b711dbc085c2f08796111c8ead1be553cede
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-19 11:32:56 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -2,14 +2,14 @@ function familiarisation() {
 return <div><h1>Answer #1</h1>
 <p>5 Most Favourite:</p>
 <p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis<br></br>2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
-confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I&#39;ve been doing this for a long and would like to continue doing so.
-<br></br>4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to &#39;one team one dream&#39; <br></br>5.Work with people to figure out if Vercel is suitable for their use case - Client use case is where it all starts + knowing the client&#39;s use case always helps in understanding how they are using our offerings.</p>
+confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I&#39;ve been doing this for a long and would like to continue doing so.
+<br></br>4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to &#39;one team one dream&#39; <br></br>5.Work with people to figure out if Vercel is suitable for their use case - Client use case is where it all starts + knowing the client&#39;s use case always helps in understanding how they are using our offerings.</p>
 
 <p>5 Least Favourite:</p>
 
 
-<p>1.Manage a support team- I am happy to lead a team but managing a team is responsibilty that I&#39;d like to avoid as at times managers struggle to be hundred percent transperent with the team. I&#39;ve respectfully declined all opportunies to be manager in the past. I&#39;d be happy to manage a team for a shorter time while we are waiting manager&#39;s position to be closed.<br></br>2.
-Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites - Something I&#39;like to avoid for now as my written commmunication is not up to mark to represent an organization at platform
+<p>1.Manage a support team- I am happy to lead a team but managing a team is responsibility that I&#39;d like to avoid as at times managers struggle to be hundred percent transparent with the team. I&#39;d be happy to manage a team for a shorter time while we are waiting manager&#39;s position to be closed.<br></br>2.
+Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites - Something I&#39;like to avoid for now as my written communication is not up to mark to represent an organization at platform
 where my comments would be read by tens of thousands of people.</p>
   
   
@@ -18,12 +18,12 @@ where my comments would be read by tens of thousands of people.</p>
   
 <h1>Answer #3</h1>
 <p>When I started my journey at Precisely (then Pitney Bowes), I used to support a product that was very widely used on Windows. The team was relatively new and since troubleshooting client cases/tickets was our primary task it was not going very well as we were unaware of tools that can present a complete picture of what is happening under the hood even when we were able to replicate the client&#39;s problem locally (It was the time when SAAS and Cloud were just emerging so all the clients were using our offering on-premises). While researching on a possible root cause
-of a problem, I came across Sysinternals tools (now part Microsoft), and just after going through their website for 10-20 minutes, I knew this is going to change our 
-(I & my team&#39;s) lives completely and it was right. Click <a href="https://docs.microsoft.com/en-us/sysinternals/" rel="noreferrer"><u>here</u></a> to go to Sysinternals home page.</p>
+of a problem, I came across Sysinternals tools (now part Microsoft), and just after going through their website for 10-20 minutes, I knew this is going to change our 
+(I & my team&#39;s) lives completely and it was right. Click <a href=https://docs.microsoft.com/en-us/sysinternals/ rel="noreferrer"><u>here</u></a> to go to Sysinternals home page.</p>
 
  <h1>Answer #4</h1>
  <p>Below link/page redirects to Vercel home.</p>
-  <a href="https://nextjs-chaudhary-gunjan.vercel.app/redirect" rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
+  <a href=https://nextjs-chaudhary-gunjan.vercel.app/redirect rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
  <br></br>
   
  <h1>Answer #5</h1>
@@ -33,7 +33,7 @@ Hello xyz<br></br><br></br>
 
 <p>Good Morning and thank you for contacting Vercel Support. Please use the information presented in one of our support articles (link below).</p>
 
-<p>Click <a href="https://vercel.com/support/articles/does-vercel-support-permanent-redirects?query=redire#other-redirects" rel="noreferrer"><u>here</u></a> to go to KB article.</p>
+<p>Click <a href=https://vercel.com/support/articles/does-vercel-support-permanent-redirects?query=redire#other-redirects rel="noreferrer"><u>here</u></a> to go to KB article.</p>
   
 <br></br>
 
@@ -44,21 +44,19 @@ Vercel Support<br></br>
 -----------------------------------------------------------------------------------------------------------------------------<br></br></p>
   
 <h1>Answer #7</h1>
-<p>Most common problems/concenrs/queries raised by Vercel clients could be related to: <br></br>
+<p>Most common problems/concerns/queries raised by Vercel clients could be related to: <br></br>
 1) DNS usage<br></br>
 2) DNS migration<br></br>
 3) Application migration from other cloud vendors (AWS etc.) to Vercel<br></br> 
-4) Billing/Cost Control - Max utilization of avalaible resources with less spending<br></br>
+4) Billing/Cost Control - Max utilization of available resources with less spending<br></br>
 5) Security/Hardening of backend infra<br></br>
 6) Issues with the Next.js code<br></br>
 7) Latency<br></br></p>
   
   <h1>Answer #8</h1>
- <p>In question #1 instead of ranking 5 most faviorite and 5 least favorite, ask can be to rank all 19 in order of one&#39;s preference. This will give us more details on candidate&#39;s preferences</p> 
+ <p>In question #1 instead of ranking 5 most favorite and 5 least favorite, ask can be to rank all 19 in order of one&#39;s preference. This will give us more details on candidate&#39;s preferences</p> 
   
 </div>
   
 }
 export default familiarisation
-
-
