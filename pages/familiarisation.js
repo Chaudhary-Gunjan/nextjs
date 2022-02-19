5f9c5daed145555d827c41e334e8265031eecabc
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-19 14:51:59 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -19,8 +19,8 @@ of a problem, I came across Sysinternals tools (now part Microsoft), and just af
 (I & my team&#39;s) lives completely and I was right. Click <a href="https://docs.microsoft.com/en-us/sysinternals/" rel="noreferrer"><u>here</u></a> to go to Sysinternals home page.</p>
 
 <h1>Answer #4</h1>
-<p>Below link/page redirects to Vercel home.</p><a href="https://nextjs-chaudhary-gunjan.vercel.app/redirect" rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
-<br></br>
+<p>Below link/page redirects to Vercel home.<br></br><a href="https://nextjs-chaudhary-gunjan.vercel.app/redirect" rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
+<br></br></p>
   
 <h1>Answer #5</h1>
 <p>If we have a support KB article already created for an issue/question, I&#39;d prefer to send the URL of the same instead of writing steps on my own. This is what I&#39;d write to the client.<br></br>
