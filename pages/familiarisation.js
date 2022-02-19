2a19181a37b60078f99794bf293e9497938a19b2
Chaudhary-Gunjan <99541896+Chaudhary-Gunjan@users.noreply.github.com>
2022-02-19 11:48:27 +0530

    Update familiarisation.js

--- pages/familiarisation.js
+++ pages/familiarisation.js
@@ -1,18 +1,15 @@
 function familiarisation() {
-return <div><h1>Answer #1</h1>
+return <div>
+<h1>Answer #1</h1>
 <p>5 Most Favourite:</p>
 <p>1.Dig through logs to troubleshoot a customer&#39;s broken project - I love troubleshooting and analysis<br></br>2.Analyze hundreds of support tickets to spot trends the product team can use - Finding one or two issues that are negatively impacting 
 confidence of a number of clients in our offering is always very helpful as most of the times they are like low hanging fruits with very high ROI.<br></br>3.Find and recruit teammates for the support team - Having the right ppl on onboard is extremely critical to the success of any organization/team. I&#39;ve been doing this for a long and would like to continue doing so.
 <br></br>4.Help train and onboard new support teammates - This does not only help team/organization but also creates a bond between teammates which is very helpful when it comes to &#39;one team one dream&#39; <br></br>5.Work with people to figure out if Vercel is suitable for their use case - Client use case is where it all starts + knowing the client&#39;s use case always helps in understanding how they are using our offerings.</p>
-
 <p>5 Least Favourite:</p>
-
-
 <p>1.Manage a support team- I am happy to lead a team but managing a team is responsibility that I&#39;d like to avoid as at times managers struggle to be hundred percent transperent with the team. I&#39;d be happy to manage a team for a shorter time while we are waiting manager&#39;s position to be closed.<br></br>2.
 Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites - Something I&#39;like to avoid for now as my written commmunication is not up to mark to represent an organization at platform
 where my comments would be read by tens of thousands of people.</p>
-  
-  
+
 <h1>Answer #2</h1>
 <p>Happy to do all the nineteen things listed in Question #1 except managing a team. Beside those, I&#39;d like to learn coding and internals of Node, React and Next.js</p>
   
@@ -21,10 +18,9 @@ where my comments would be read by tens of thousands of people.</p>
 of a problem, I came across Sysinternals tools (now part Microsoft), and just after going through their website for 10-20 minutes, I knew this is going to change our 
 (I & my team&#39;s) lives completely and I was right. Click <a href="https://docs.microsoft.com/en-us/sysinternals/" rel="noreferrer"><u>here</u></a> to go to Sysinternals home page.</p>
 
- <h1>Answer #4</h1>
- <p>Below link/page redirects to Vercel home.</p>
-  <a href="https://nextjs-chaudhary-gunjan.vercel.app/redirect" rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
- <br></br>
+<h1>Answer #4</h1>
+<p>Below link/page redirects to Vercel home.</p><a href="https://nextjs-chaudhary-gunjan.vercel.app/redirect" rel="noreferrer"><u>https://nextjs-chaudhary-gunjan.vercel.app/redirect</u></a>
+<br></br>
   
  <h1>Answer #5</h1>
  <p>If we have a support KB article already created for an issue/question, I&#39;d prefer to send the URL of the same instead of writing steps on my own. This is what I&#39;d write to the client.<br></br>
